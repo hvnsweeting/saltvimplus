6c4c57302d8e664c5565bdabdf57aced84cad561
hvnsweeting <hvnsweeting@gmail.com>
2013-09-23 22:15:22 +0700

    backup existing .vim dir

--- install.sh
+++ install.sh
@@ -8,12 +8,17 @@ cd $tempdir
 git clone git://github.com/saltstack/salt-vim.git
 git clone git://github.com/hvnsweeting/snipmate.vim.git
 git clone git://github.com/Townk/vim-autoclose.git
+
 if [ ! -d ~/.vim ]; then
-  mkdir ~/.vim
+    mkdir ~/.vim
+else
+    cp -r ~/.vim /tmp/dotvim_`date +%Y%m%d_%H%M%S`
 fi
+
 for dir in *; do
     cp -r $dir/* ~/.vim
 done
+
 wget https://raw.github.com/hvnsweeting/hvnrc/master/vimrc
 cp vimrc ~/.vimrc
 rm -rf $tempdir
